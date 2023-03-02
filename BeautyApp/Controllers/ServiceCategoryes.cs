83c38b8b1910dbf97abf98fa397094044ad818ad
KatsumiF <Valya.i.persik@gmail.com>
2023-03-02 15:48:23 +0500

    Вывод категорий

---
+++ BeautyApp/Controllers/ServiceCategoryes.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace BeautyApp.Controllers
+{
+    public class ServiceCategoryes
+    {/// <summary>
+    /// Вывод категорий
+    /// </summary>
+        public List<ServiceCategoryes> GetServiceCategoryes()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"{Manager.RootUrl}ServiceCategoryes";
+                Console.WriteLine(url);
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                var content = response.Content.ReadAsStringAsync();
+                var answer = JsonConvert.DeserializeObject<List<ServiceCategoryes>>(content.Result);
+                return answer;
+            }
+        }
+    }
+}
---
+++ BeautyApp/Models/ServiceCategoryes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyApp.Models
+{
+    public class ServiceCategoryes
+    {
+
+        public class Rootobject
+        {
+            public int CategoryId { get; set; }
+            public string CategoryTitle { get; set; }
+            public byte[] CategoryImage { get; set; }
+        }
+
+    }
+}
